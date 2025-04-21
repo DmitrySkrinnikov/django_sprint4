684ceac28ead26becd43f5dcb18b62029b058f0b
Dmitry Skrinnikov <skrinnikov.d@yandex.ru>
2025-04-21 19:23:47 +0300

    PEP 8. Второй запуск теста.

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse_lazy, reverse
 from django.utils import timezone
